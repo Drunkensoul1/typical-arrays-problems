c7d6f8ee97d2b3a3c8f6457a8c88a250773b9ded
Egor Lifianets <lifenetsegor@gmail.com>
2021-03-16 01:15:26 +0300

    chd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minVal = 0;
+  for (let i=0; i < array.length; i++) {
+    
+    if (minVal > array[i]) {
+       minVal = array[i];
+    }else if (array.length == 0) {
+      minVal = 0;
+    }
+    
+}
+return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxVal = 0;
+  const length = array.length;
+  for (let i=0; i < length; i++) {
+    
+    if (maxVal < array[i]) {
+       maxVal = array[i];
+    } else if (length === 0)  {
+      return 0;
+    }
+    
+  }
+return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let j = 0;
+  for (let i=0; i < array.length; i++) {
+    j += array[i];
+       if (array.length == 0){
+        return 0;
+       }
+  
+}
+return j/array.length;
 }
